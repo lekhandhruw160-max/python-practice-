b3577ed926e4db50895406c13f6357365cb386ec
lekhandhruw160-max <lekhandhruw160@gmail.com>
2026-02-09 19:48:07 +0530

    add new practice code

--- first.py
+++ first.py
@@ -1,11 +1,62 @@
-x = int(input("enter your favorite number :"))   
+"""x = int(input("enter your favorite number :"))   
 
 tuples =  (1,4 ,9, 16, 25, 36, 49, 64, 81, 100 )
 i = 0 
 while i < len(tuples):
     if x == tuples [i]: 
         print(x ,"found at the indx : ", i)
+    i += 1 """
+# python loop practice 
+
+"""i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+for i in range(11):
+    print(i)
+i = 1
+while i <= 101:
+    if i%2 == 0:
+        print(i)
+    i += 1
+num = 2
+for i in range(50):
+    if num%2 == 0:
+      print(num * i)
+
+i = 10
+while i > 0:
+    print(i)
+    i -= 1 
+
+ 
+nums = int(input("enter a some number :"))   
+i = 1
+while i<= 10:
+    print(i * nums)
     i += 1 
+
+num = int(input("enter some number :" ))
+for i in range(1,11):
+    print(i*num)
+list = [2, 3, 4, 4, 5, 8, 6]
+for i in range(len(list)):
+    print(list)
+
+list = "lekhandhruw"
+indx = 1
+for val in list:
+    print(val , indx )
+    indx += 1'"""
+
+
+i = 1 
+fact = 0 
+while i <= 50:
+    fact += i
+    print(fact)
+    i += 1
 
 
 
